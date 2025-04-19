0201387831282b4d755df61317be0aa87335d60e
Elvis Gomez <eogomezf@gmail.com>
2025-04-18 19:58:11 -0600

    Crear componente producto usando props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css";
+import { Cart } from "./components/Cart";
 import Product from "./components/Product";
 
 function App() {
+  const style = {
+    marginTop: "100px",
+    marginLeft: "120px",
+    marginRight: "120px",
+  };
+
   return (
-    <>
-      <Product />
-    </>
+    <div className="container pt-25 align-items-center" style={style}>
+      <div className="row">
+        <div className="col-9">
+          <h1>Desserts</h1>
+          <div className="d-flex flex-wrap m-5">
+            <Product
+              name={"Flang"}
+              category={"Wafle"}
+              price={10}
+              img={"src/./assets/images/image-brownie-desktop.jpg"}
+            />
+          </div>
+        </div>
+        <div className="col-3">
+          <Cart />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,34 @@
-function Product() {
-  return <h1>producto</h1>;
+interface ProductProps {
+  name: string;
+  category: string;
+  price: number;
+  img: string;
+}
+
+function Product(props: ProductProps) {
+  const { name, category, price, img } = props;
+  const width = {
+    width: "14rem",
+    borderRadius: "10px",
+  };
+  // const img: string = "src/assets/images/image-brownie-desktop.jpg";
+  //const name: string = "Product Name";
+  //const price: number = 6.5;
+
+  return (
+    <>
+      <div className="d-flex flex-column m-3 ">
+        <img className="border" src={img} alt={name} style={width} />
+        <div className="mt-3 d-flex flex-column">
+          <small>{category}</small>
+          <span>
+            <strong>{name}</strong>
+          </span>
+          <p className="text-danger">$ {price}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Product;
